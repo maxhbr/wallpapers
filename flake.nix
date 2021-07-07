bdd6065b63baf4d1dfcd3a9d383ef8856c8faed1
Maximilian Huber <gh@maxhbr.de>
2021-07-07 16:38:17 +0200

    add flakes
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "my wallpapers";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs, ... }@inputs: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    my-wallpapers = pkgs.callPackage ./default.nix {};
+  in {
+    defaultPackage.x86_64-linux = my-wallpapers;
+    nixosModule = { config, lib, pkgs, ... }: {
+      config = (lib.mkIf config.services.xserver.enable {
+        home-manager.sharedModules = [{
+          home.packages = [ my-wallpapers ];
+          services.random-background = {
+            enable = true;
+            imageDirectory = "${my-wallpapers}/share";
+            display = "scale";
+            interval = "10min";
+          };
+        }];
+
+        services.xserver.displayManager.lightdm = {
+          background = "${my-wallpapers}/share/romben3.png";
+          # sessionCommands = ''
+          #   ${pkgs.my-wallpapers}/bin/myRandomBackground &disown
+          # '';
+        };
+      });
+    };
+  };
+}
